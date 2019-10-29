8f358542d756318197401db12286235cde5287e6
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-10-29 18:25:57 -0400

    made code R00 compliant

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,10 +1,15 @@
 //rule 00
 //Input Validation and Data Sanitization (IDS) example 03
-//noncompliant solution
+//compliant solution
 
 
 if (loginSuccessful){
-	logger.severe("User login Succeeded for: " + username);
+  	logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-	logger.severe("User login failed for: " + username);
+  	logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
 }
