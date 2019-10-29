8b90786504b76e4045379c62b477b42b948be49b
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-10-29 18:27:52 -0400

    made code R02 compliant

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,11 +1,13 @@
 //rule 02
 //Expressions (XP) example 00
-//noncompliant solution
+//compliant solution
 
 
 public void deleteFile(){
-
-	File someFile = new File ("someFileName.txt");
-	//Do something with someFile
-	someFile.delete();
-}
+ 
+  	File someFile = new File("someFileName.txt");
+  	// Do something with someFile
+  	if (!someFile.delete()) {
+    		// Handle failure to delete the file
+  	}
+ }
