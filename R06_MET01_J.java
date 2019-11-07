36534c360c8050dc645a05086be08890de7c2685
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 12:31:53 -0500

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -2,13 +2,17 @@
 //Methods (MET) example 01
 //noncompliant solution
 
-
-public static int getAbsAdd(int x, int y){
-	assert x != Integer.MIN_VALUE;
-	assert y != Integer.MIN_VALUE;
-	int absX = Math.abs(x);
-	int absY = Math.abs(y);
-	assert (absX <= Integer.MAX_VALUE - absY);
-	return absX + absY;
+public class R06_Met01_J {
+	
+	public static int getAbsAdd(int x, int y){
+		assert x != Integer.MIN_VALUE;
+		assert y != Integer.MIN_VALUE;
+		int absX = Math.abs(x);
+		int absY = Math.abs(y);
+		assert (absX <= Integer.MAX_VALUE - absY);
+		return absX + absY;
+	}
+	
+	
 }
 //Usage: getAbsAdd(Integer.MIN_VALUE, 1);
