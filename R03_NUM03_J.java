6f56e165d5f842967897fe2504cb72e5c4fa7a1e
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 17:32:28 -0500

    made to compile
    Compiles, executes, but stalls before completion

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,8 +1,20 @@
-//rule 03
-//Numeric Types and Operations (NUM) example 03
-//noncompliant solution
+/***********************************************
+Rule 03
+Numeric Types and Operations (NUM) example 03
+Compilation:   javac R03_NUM03_J.java
+Execution:     java R03_NUM03_J
+noncompliant solution
+************************************************/
+import java.io.*;
 
+public class R03_NUM03_J{
 
-public static int getInteger(DataInputStream is) throws IOException {
-	return is.readInt();
+	public static void main(String[] args) throws IOException {
+		DataInputStream is = new DataInputStream(System.in);
+		System.out.println("Integer =" + getInteger(is));
+	}
+
+	public static int getInteger(DataInputStream is) throws IOException {
+		return is.readInt();
+	}
 }
