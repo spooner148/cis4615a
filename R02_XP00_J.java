9644f296c19b535c4dbe6480610373423fb07361
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 18:19:59 -0500

    made to compile

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,13 +1,29 @@
-//rule 02
-//Expressions (XP) example 00
-//compliant solution
+/***************************************
+Rule 02
+Expressions (XP) example 00
+Compilation:   javac R02_XP00_J.java
+Execution:     java R02_XP00_J
+compliant solution
+****************************************/
+import java.io.*;
 
+public class R02_XP00_J {
 
-public void deleteFile(){
- 
-  	File someFile = new File("someFileName.txt");
-  	// Do something with someFile
-  	if (!someFile.delete()) {
-    		// Handle failure to delete the file
-  	}
+	public static void main(String[] args){
+		deleteFile();
+	}
+
+	public static void deleteFile() {
+
+		File someFile = new File ("someFileName.txt");
+		//Do something with someFile
+		System.out.println(someFile +" created");
+		
+		if (!someFile.delete()) {
+    		System.out.println(someFile + " not deleted successfully");
+  } else {
+			   System.out.println(someFile + " successfully deleted");
+	 }
  }
+}
+ 
