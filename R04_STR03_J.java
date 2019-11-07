55ab5ca8fe68b520eb8fe5631af9e33d3fc21528
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 13:36:05 -0500

    made to compile

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,10 +1,22 @@
-//rule 04
-//Characters and Strings (STR) example 03
-//noncompliant solution
+/*******************************************
+Rule 04
+Characters and Strings (STR) example 03
+Compilation:   javac R04_STR03_J.java
+Execution:     java R04_STR03_J
+noncompliant solution
+*******************************************/
+import java.math.BigInteger;
 
+public class R04_STR03_J {
 
-BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+    public static void main(String[] args){
+
+    BigInteger x = new BigInteger("530500452766");
+    byte[] byteArray = x.toByteArray();
+    String s = new String(byteArray);
+    byteArray = s.getBytes();
+    x = new BigInteger(byteArray);
+
+    System.out.println("x = "+ x);
+    }
+}
