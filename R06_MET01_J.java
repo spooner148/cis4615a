e2f21e6f6af311b3b8429da5a6c5ae19544724ab
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 13:24:26 -0500

    made to compile

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,16 +1,31 @@
-//rule 06
-//Methods (MET) example 01
-//compliant solution
+/*rule 06
+Methods (MET) example 01
+Compilation: javac R06_MET01_J.java
+Execution: jaca R06_MET01_J
+*/
 
+public class R06_MET01_J {
+  
+  	public static void main(String[] args){
 
-public static int getAbsAdd(int x, int y) {
-  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
-    throw new IllegalArgumentException();
+		int a = 17;
+		System.out.println("a = "+a);
+		int b = 42;
+		System.out.println("b = "+b);
+		int c = getAbsAdd(a,b);
+		System.out.println("c = "+c);
+	}
+  
+  //compliant solution
+  public static int getAbsAdd(int x, int y) {
+    if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+      throw new IllegalArgumentException();
+    }
+    int absX = Math.abs(x);
+    int absY = Math.abs(y);
+    if (absX > Integer.MAX_VALUE - absY) {
+      throw new IllegalArgumentException();
+    }
+    return absX + absY;
   }
-  int absX = Math.abs(x);
-  int absY = Math.abs(y);
-  if (absX > Integer.MAX_VALUE - absY) {
-    throw new IllegalArgumentException();
-  }
-  return absX + absY;
 }
