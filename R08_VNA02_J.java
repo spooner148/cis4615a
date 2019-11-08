e186a202f859fde5c64f0e0361e2138a8076c44d
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 20:42:21 -0500

    made to compile

--- R08_VNA02_J.java
+++ R08_VNA02_J.java
@@ -1,7 +1,10 @@
-//rule 08
-//Visibility and Atomicity (VNA) example 02
-//compliant solution
-
+/*****************************************
+Rule 08
+Visibility and Atomicity (VNA) example 02
+Compilation:    javac R08_VNA08_J.java
+Execution: 	    java R08_VNA08_J
+compliant solution
+*****************************************/
 
 final class Flag {
   private boolean flag = true;
@@ -13,4 +16,35 @@ final class Flag {
   public synchronized boolean getFlag() {
     return flag;
   }
+}
+
+class multi implements Runnable{
+  private Flag flag;
+
+  public multi(Flag flag){
+    this.flag = flag;
+  }
+
+  @Override
+  public void run(){
+    flag.toggle();
+    System.out.println("thread is running...");
+  }
+}
+
+public class R08_VNA02_J {
+  public static void main(String[] args){
+    Flag flag = new Flag();
+
+    Thread t1 = new Thread(new multi(flag));
+    Thread t2 = new Thread(new multi(flag));
+    t1.start();
+    t2.start();
+    try {
+      t1.join();
+      t2.join();
+    } catch (Exception e) {}
+
+  }
+
 }
