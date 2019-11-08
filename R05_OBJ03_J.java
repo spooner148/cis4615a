cf85cb4ef145894d6233f2835fbf0baec6e181af
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 19:47:45 -0500

    made to compile

---
+++ R05_OBJ03_J.java
@@ -0,0 +1,23 @@
+/*****************************************
+Rule 05
+Object Orientation (OBJ) example 03
+Compilation:    javac R05_OBJ03_J.java
+Execution: 	    java R05_OBJ03_J
+noncompliant solution
+*****************************************/
+import java.util.ArrayList;
+import java.util.List;
+
+//public class R05_OBJ03_J {
+  
+public class R05_OBJ03_J {
+  private static void addToList(List list, Object obj) {
+    list.add(obj); // Unchecked warning
+  }
+ 
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<String> ();
+    addToList(list, "42");
+    System.out.println(list.get(0));  // Throws ClassCastException
+  }
+}
