5f208f59735182f7e277ce2470fe072e50b6dda7
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 20:17:25 -0500

    made to compile

--- R07_ERR01_J.java
+++ R07_ERR01_J.java
@@ -1,13 +1,26 @@
-//rule 07
-//Exceptional Behavior (ERR) example 01
-//noncompliant solution
+/***********************************************
+Rule 07
+Exceptional Behavior (ERR) example 01
+Compilation:   javac R07_ERR01_J.java
+Execution:     java R07_ERR01_J
+noncompliant solution
+************************************************/
+import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
+public class R07_ERR01_J {
+	public static void main(String[] args) throws FileNotFoundException {
+   
+		System.out.print("Enter file name:");
+		Scanner in = new Scanner(System.in);
+		String filename = in.nextLine();
 
-class ExceptionExample {
-  public static void main(String[] args) throws FileNotFoundException {
-    // Linux stores a user's home directory path in
-    // the environment variable $HOME, Windows in %APPDATA%
-    FileInputStream fis =
-        new FileInputStream(System.getenv("APPDATA") + args[0]); 
-  }
-}
+		// Linux stores a user's home directory path in
+		// the environment variable $HOME, Windows in %APPDATA%
+		FileInputStream fis =
+			new FileInputStream(System.getenv("APPDATA") + filename); 
+		}
+	
+	}
+  
