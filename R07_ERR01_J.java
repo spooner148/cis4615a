15ef6db46d84147ede3cb0716f8dec8ffcb718a7
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 20:12:52 -0500

    made to compile

--- R07_ERR01_J.java
+++ R07_ERR01_J.java
@@ -1,29 +1,40 @@
-//rule 07
-//Exceptional Behavior (ERR) example 01
-//compliant solution
+/***********************************************
+Rule 07
+Exceptional Behavior (ERR) example 01
+Compilation:   javac R07_ERR01_J.java
+Execution:     java R07_ERR01_J
+compliant solution
+************************************************/
+import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
+public class R07_ERR01_J {
+	public static void main(String[] args) {
+   
+		System.out.print("Enter file name:");
+		Scanner in = new Scanner(System.in);
+		String filename = in.nextLine();
 
-class ExceptionExample {
-  public static void main(String[] args) {
- 
-    File file = null;
-    try {
-      file = new File(System.getenv("APPDATA") +
-             args[0]).getCanonicalFile();
-      if (!file.getPath().startsWith("c:\\homepath")) {
-        System.out.println("Invalid file");
-        return;
-      }
-    } catch (IOException x) {
-      System.out.println("Invalid file");
-      return;
-    }
- 
-    try {
-      FileInputStream fis = new FileInputStream(file);
-    } catch (FileNotFoundException x) {
-      System.out.println("Invalid file");
-      return;
-    }
+	  File file = null;
+	  
+	  try {
+		file = new File(System.getenv("APPDATA") + filename).getCanonicalFile();
+		if (!file.getPath().startsWith("c:\\homepath")) {
+		  System.out.println("Invalid file");
+		  return;
+		}
+	  } catch (IOException x) {
+		System.out.println("Invalid file");
+		return;
+	  }
+   
+	  try {
+		FileInputStream fis = new FileInputStream(file);
+	  } catch (FileNotFoundException x) {
+		System.out.println("Invalid file");
+		return;
+	  }
+	  in.close();
+	}
   }
-}
