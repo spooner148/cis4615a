5e3be7100dade24479d926f7130898f99de602f5
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 19:51:36 -0500

    made to compile (still stalling)

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,8 +1,23 @@
-//rule 03
-//Numeric Types and Operations (NUM) example 03
-//compliant solution
+/***********************************************
+Rule 03
+Numeric Types and Operations (NUM) example 03
+Compilation:   javac R03_NUM03_J.java
+Execution:     java R03_NUM03_J
+compliant solution
+************************************************/
+import java.io.*;
 
+public class R03_NUM03_J{
+	
+	public static void main(String[] args) throws IOException {
 
-public static long getInteger(DataInputStream is) throws IOException {
-  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+		System.out.println("check1");
+		DataInputStream is = new DataInputStream(System.in);
+		System.out.println("Integer =" + getInteger(is));
+		is.close();
+	}
+
+	public static long getInteger(DataInputStream is) throws IOException {
+		return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+	  }
 }
