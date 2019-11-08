fff163ce7193e3d539382ed3c807cff39cec3539
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 22:09:54 -0500

    made to compile

--- R10_THI00_J.java
+++ R10_THI00_J.java
@@ -1,15 +1,19 @@
-//rule 10
-//Thread APIs (THI) example 00
-//compliant solution
+/*****************************************
+Rule 10
+Thread APIs (THI) example 00
+Compilation:    javac R10_THI00_J.java
+Execution: 	    java R10_THI00_J
+compliant solution
+*****************************************/
 
-
-public final class Foo implements Runnable {
+public final class R10_THI00_J implements Runnable {
+  
   @Override public void run() {
-    // ...
+    System.out.println("running thread.");
   }
  
   public static void main(String[] args) {
-    Foo foo = new Foo();
+    R10_THI00_J foo = new R10_THI00_J();
     new Thread(foo).start();
   }
 }
