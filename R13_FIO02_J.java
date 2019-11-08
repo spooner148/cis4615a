68db9a805be8736dae1ce804aafbd9603c85bcb6
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 21:59:37 -0500

    made to compile

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -1,7 +1,18 @@
-//rule 13
-//Input Output (FIO) example 02
-//noncompliant solution
+/*****************************************
+Rule 13
+Input Output (FIO) example 02
+Compilation:    javac R13_FIO02_J.java
+Execution: 	    java R13_FIO02_J
+noncompliant solution
+*****************************************/
+import java.io.File;
 
-
-File file = new File(args[0]);
-file.delete();
+public class R13_FIO02_J {
+    
+    public static void main(String[] args){
+       
+        File file = new File("filename.txt");
+        file.delete();
+        System.out.println("File deleted probably.");
+    }
+}
