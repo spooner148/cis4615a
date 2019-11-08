69e2e3d8069990aefda7e645641061fd649b216c
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 19:07:11 -0500

    made to compile

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,10 +1,32 @@
-//rule 00
-//Input Validation and Data Sanitization (IDS) example 03
-//noncompliant solution
+/*********************************************************
+Rule 00
+Input Validation and Data Sanitization (IDS) example 03
+Compilation:   javac R00_IDS03_J.java
+Execution:     java R00_IDS03_J
+noncompliant solution
+**********************************************************/
+import java.util.regex.Pattern;
+import java.util.logging.*;
+import java.util.Scanner;
 
+public class R00_IDS03_J {
 
-if (loginSuccessful){
-	logger.severe("User login Succeeded for: " + username);
-} else {
-	logger.severe("User login failed for: " + username);
+	static Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+
+	public static void main(String[] args) {
+		boolean loginSuccessful = true;
+
+		//get username
+		System.out.print("Enter Username:");
+		Scanner in = new Scanner(System.in);
+		String username = in.nextLine();
+
+		if (loginSuccessful){
+			logger.severe("User login Succeeded for: " + username);
+		} else {
+			logger.severe("User login failed for: " + username);
+		}
+		
+		in.close();
+	}
 }
