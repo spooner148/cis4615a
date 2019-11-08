70c78dfff16e717ab08ac075d5e5f60c3c6ce563
spooner148 <39808901+spooner148@users.noreply.github.com>
2019-11-07 22:01:34 -0500

    made to compile

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -1,9 +1,21 @@
-//rule 13
-//Input Output (FIO) example 02
-//compliant solution
+/*****************************************
+Rule 13
+Input Output (FIO) example 02
+Compilation:    javac R13_FIO02_J.java
+Execution: 	    java R13_FIO02_J
+compliant solution
+*****************************************/
+import java.io.File;
 
-
-File file = new File("file");
-if (!file.delete()) {
-  System.out.println("Deletion failed");
+public class R13_FIO02_J {
+    
+    public static void main(String[] args){
+       
+        File file = new File("filename.txt");
+        if (!file.delete()) {
+            System.out.println("Deletion failed.");
+        } else {
+        System.out.println("File deleted.");
+        }
+    }
 }
